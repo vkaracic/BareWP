59eba7a2b3a18730f8e72a1b20e08d8862eb1d55
vkaracic <karacicvedran@gmail.com>
2015-11-03 21:03:32 +0100

    Created starter files

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+
+</footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<div id="main-container">
+
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>

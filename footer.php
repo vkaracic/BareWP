39e6eb84ad4ab8a3070745d74c95261eefa8445a
vkaracic <karacicvedran@gmail.com>
2015-11-08 19:45:16 +0100

    Added style.css and main.js files

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
 <footer>
-
+FOOTER
 </footer>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="wp-content/themes/BareWP/main.js"></script>
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -6,5 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BareWP theme</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link href="wp-content/themes/BareWP/style.css" rel="stylesheet">
 </head>
 <body>
+<header>
+<?php show_nav("", "test"); ?>
+</header>

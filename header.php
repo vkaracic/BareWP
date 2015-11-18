53f5eb74bac2c577c18d84299e42da851d0701e4
vkaracic <karacicvedran@gmail.com>
2015-11-18 23:48:19 +0100

    All media queries

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
     <title>BareWP theme</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
     <link href="wp-content/themes/BareWP/style.css" rel="stylesheet">
+    <link href="wp-content/themes/BareWP/responsive.css" rel="stylesheet">
 </head>
 <body>
 <header>

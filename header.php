960aa68c88e783d7e50ec9a104cd2bab13bcd9ab
vkaracic <karacicvedran@gmail.com>
2015-11-25 19:31:53 +0100

    Add Open Sans import.

--- header.php
+++ header.php
@@ -8,6 +8,10 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
     <link href="wp-content/themes/BareWP/style.css" rel="stylesheet">
     <link href="wp-content/themes/BareWP/responsive.css" rel="stylesheet">
+
+    <!-- Google fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    
 </head>
 <body>
 <header>
